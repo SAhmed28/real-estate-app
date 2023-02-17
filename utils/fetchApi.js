18015ebd8aa661d5453d3aa06d836dd966d985e2
SAhmed28 <sanjida1@ualberta.ca>
2023-02-16 19:37:01 -0700

    fixed one issue

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': 'cad1c57c1bmsh1bd711ec1bed917p1cd1e5jsnddb3ae8a9122',
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
           }
     });
